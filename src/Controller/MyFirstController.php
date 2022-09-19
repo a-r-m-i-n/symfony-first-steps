f38e3e5383b14bec7e371082b0911ce5ab505319
Armin Vieweg <info@v.ieweg.de>
2022-09-19 18:10:35 +0200

    [FEATURE][2/2] Accessing ParameterBag values and using debug helper
    Values configured in "parameters"-section of services.yaml are managed
    via the ParameterBag. We can inject this ParameterBag to our e.g. Controller and access its values.
    
    Also in this example, one value is being output in MyFirstController, using the "dump" PHP function. Values tracked with this function, can get viewed in the Debug profiler toolbar.

--- src/Controller/MyFirstController.php
+++ src/Controller/MyFirstController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Service\RandomNumberGeneratorService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
@@ -11,7 +12,7 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 class MyFirstController extends AbstractController
 {
     #[Route('/', name: 'app_welcome')]
-    public function welcome(RandomNumberGeneratorService $randomNumberGeneratorService): Response
+    public function welcome(RandomNumberGeneratorService $randomNumberGeneratorService, ParameterBagInterface $parameterBag): Response
     {
         $absoluteUrlToRoute = $this->generateUrl(
             'app_pizza_essen',
@@ -19,10 +20,13 @@ class MyFirstController extends AbstractController
             UrlGeneratorInterface::ABSOLUTE_URL
         );
 
+        dump('Max number in parameter bag is: ' . $parameterBag->get('app.max_number'));
+
         return $this->render('welcome/index.html.twig', [
             'controller_name' => 'MyFirstController',
             'absolute_url_to_route' => $absoluteUrlToRoute,
             'random_number' => $randomNumberGeneratorService->generate(),
+            'configuredParameters' => $parameterBag->all(),
         ]);
     }
 }
