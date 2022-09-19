081a09add65d8289c43eaaa8f27104232bdd09a8
Armin Vieweg <info@v.ieweg.de>
2022-09-19 17:55:53 +0200

    [TASK] Generate URLs to routes, programmatically
    in Controller and in Twig template directly

--- src/Controller/MyFirstController.php
+++ src/Controller/MyFirstController.php
@@ -5,14 +5,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class MyFirstController extends AbstractController
 {
     #[Route('/', name: 'app_welcome')]
     public function welcome(): Response
     {
+        $absoluteUrlToRoute = $this->generateUrl(
+            'app_pizza_essen',
+            ['sorte' => 'Quattro Formaggi'],
+            UrlGeneratorInterface::ABSOLUTE_URL
+        );
+
         return $this->render('welcome/index.html.twig', [
             'controller_name' => 'MyFirstController',
+            'absolute_url_to_route' => $absoluteUrlToRoute,
         ]);
     }
 }
