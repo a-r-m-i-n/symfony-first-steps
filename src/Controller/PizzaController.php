1125d6af8e520286320f3887df0b2475582aceb9
Armin Vieweg <info@v.ieweg.de>
2022-09-19 17:37:03 +0200

    [FEATURE] Add new PizzaController with two actions
    The "index" action redirects to "essen" action,
    which has a dynamic parameter ("sorte") configured. This parameter is being used in controller action and output in view.
    
    Variables passed to and used in Twig templates, are escaped by default, which prevents XSS (Cross Site Scripting).

---
+++ src/Controller/PizzaController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/pizza', name: 'app_pizza_')]
+class PizzaController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->redirectToRoute('app_pizza_essen');
+    }
+
+    #[Route('/essen/{sorte}', name: 'essen')]
+    public function essen(string $sorte = 'Margherita'): Response
+    {
+        return $this->render('pizza/essen.html.twig', [
+            'sorte' => ucfirst(trim($sorte)),
+        ]);
+    }
+}
