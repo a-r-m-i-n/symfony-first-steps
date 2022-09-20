557b793c0d1a707222e193400618568ef2f2292e
Armin Vieweg <info@v.ieweg.de>
2022-09-20 09:26:48 +0200

    [FEATURE] Create new Symfony bundle and require it in root composer.json

---
+++ bundles/example-bundle/src/ExampleBundle.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types = 1);
+namespace Armin\ExampleBundle;
+
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class ExampleBundle extends AbstractBundle
+{
+}
--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Armin\ExampleBundle\ExampleBundle::class => ['all' => true],
 ];
